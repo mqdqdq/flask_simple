be1159a9c362405eb0a6bc22dc18e07c28de4828
eha261x <eha261@uit.no>
2024-06-23 16:52:40 +0200

    fixed room sockets broadcasting to individual rooms

--- sockets.py
+++ sockets.py
@@ -1,4 +1,4 @@
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask import session
 from database.db import ChatDb
 
@@ -11,7 +11,7 @@ def on_join():
         with ChatDb() as db:
             db.add_user_to_room(room_id)
             _, _, user_count = db.get_room(room_id)
-        emit('client-connect', user_count, broadcast=True)
+        emit('client-connect', user_count, join_room(room_id))
 
 @socketio.on('disconnect')
 def on_leave():
@@ -20,7 +20,7 @@ def on_leave():
         with ChatDb() as db:
             db.remove_user_from_room(room_id)
             _, _, user_count = db.get_room(room_id)
-        emit('client-disconnect', user_count, broadcast=True)
+        emit('client-disconnect', user_count, leave_room(room_id))
 
 @socketio.on('server-new-message')
 def message(message):
@@ -28,4 +28,4 @@ def message(message):
     if room_id:
         with ChatDb() as db:
             db.add_chat_entry(room_id, message)
-        emit('client-new-message', message, broadcast=True)
+        emit('client-new-message', message, to=room_id)
